6bd933db5f659ac0b1932ddb887607c3b50ac5d6
yilmaz1998 <snn.ylmz9804@gmail.com>
2024-04-13 16:05:12 -0400

    order schema and routes

--- controllers/orderCtrl.js
+++ controllers/orderCtrl.js
@@ -1,23 +1,49 @@
 const Order = require("./../models/order");
 
-const createOrder = (req, res) => {
-  res.send("create an order");
+const createOrder = async (req, res) => {
+  try {
+    const newOrder = new Order(req.body)
+    await newOrder.save()
+    res.status(201).json(newOrder)
+  }catch (err) {
+    res.status(400).json({message: err.message})
+  }
 };
 
-const getUserOrders = (req, res) => {
-  res.send("get orders of a single user");
+const getUserOrders = async (req, res) => {
+  try {
+    const userOrders = await Order.find({ user: req.params.userId }).populate('orders').populate('user');
+    res.status(201).json(userOrders);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
-const deleteOrder = (req, res) => {
-  res.send("delete an order");
+const deleteOrder = async (req, res) => {
+  try {
+    const deletedOrder = await Order.findByIdAndDelete(req.params.id)
+    res.status(201).json(deletedOrder)
+  } catch (err) {
+    res.status(400).json({message: err.message})
+  }
 };
 
-const showOrder = (req, res) => {
-  res.send("show an order");
+const showOrder = async (req, res) => {
+  try {
+    const show = await Order.findById(req.params.id).populate('orders').populate('user')
+    res.status(201).json(show)
+  } catch (err) {
+    res.status(400).json({message: err.message})
+  }
 };
 
-const updateOrder = (req, res) => {
-  res.send("update order");
+const updateOrder = async (req, res) => {
+  try {
+    const update = await Order.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.status(201).json(update)
+  } catch (err) {
+    res.status(400).json({message: err.message})
+  }
 };
 
 module.exports = {
